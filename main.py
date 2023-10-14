2237739edd3a69830a38be1391b4cfd1134bce8a
domi-pro <dominika.prokocka@gmail.com>
2023-10-14 18:58:13 +0200

    edit: login, signup error messages,  add: homepage

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import sqlite3
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, flash
 
 # App setup
 app = Flask(__name__)
+app.secret_key = 'many random bytes'
 
 # Connections to databases
 connection_flashcard = sqlite3.connect("fiszki.db");
@@ -15,9 +16,7 @@ first = rows[0];
 # Routes
 @app.route('/')
 def index():
-    pol_def = str(rows[0][1])
-    hiszp_def = str(rows[0][2])
-    return render_template('index.html', pol_def=pol_def, hiszp_def=hiszp_def);
+    return render_template('index.html');
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -35,9 +34,10 @@ def login():
         results = cursor_user.fetchall()
 
         if len(results) == 0:
-           return render_template('login_false.html')
+           flash("Dane logowania są niepoprawne. Spróbuj ponownie")
+           return redirect(url_for('login'))
         else:
-            return render_template('login_correct.html')
+            return redirect(url_for('home'))
     return render_template('login.html')
 
 @app.route('/signup', methods=['GET', 'POST'])
@@ -56,10 +56,17 @@ def signup():
             add_to_base = "INSERT INTO users VALUES('"+username+"', '"+password+"')"
             cursor_user.execute(add_to_base)
             connection_user.commit()
-            return render_template('registration_correct.html')
+            return render_template('login.html')
         else:
-            return render_template('registration_incorrect.html')
+            flash("Użytkownik o takiej nazwie już istnieje. Spróbuj ponownie")
+            return redirect(url_for('signup'))
     return render_template('signup.html')
+
+@app.route('/home')
+def home():
+    pol_def = str(rows[0][1])
+    hiszp_def = str(rows[0][2])
+    return render_template('home.html', pol_def=pol_def, hiszp_def=hiszp_def)
 
 
 if __name__ == "__main__":
