ebbc1998f99a76ca99c6ea1f3f48a8b8d7a8ec99
domi-pro <dominika.prokocka@gmail.com>
2023-10-24 23:34:54 +0200

    add: password change and user model

--- main.py
+++ main.py
@@ -1,9 +1,44 @@
+### NOTATKI:
+
+## Kolejny krok to dodać zabezpieczenie wymagania zalogowania zeby przejsc do strony home i wyswietlac uzytkownika
+## Jest do tego biblioteka LoginManager ale wymaga to ode mnie operacji na obiekcie User i haszowaniu hasła
+## Dlatego muszę przerobić kod tak aby działał na obiekcie User to jego dodawał do bazy oraz posiadał haszowanie hasła
+## Potem kolejne kroki aby użyć LoginManagera 
+
 import sqlite3
 from flask import Flask, render_template, redirect, url_for, request, flash
-
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 # App setup
 app = Flask(__name__)
-app.secret_key = 'many random bytes'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SECRET_KEY'] = 'many random bytes'
+
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_message = "Zaloguj się, aby wejść na stronę"
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
+
+class Users(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(200), nullable=False, unique=True)
+    password = db.Column(db.String(200), nullable=False, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Name %r>' % self.username
+
+with app.app_context():
+    db.create_all()
 
 # Connections to databases
 connection_flashcard = sqlite3.connect("fiszki.db");
@@ -21,53 +56,87 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-
-        connection_user = sqlite3.connect("users_dane.db");
-        cursor_user = connection_user.cursor();
     
         username = request.form['username']
         password = request.form['password']
 
-        query = "SELECT username, password FROM users WHERE username='"+username+"' AND password ='"+password+"'"
-        cursor_user.execute(query)
-
-        results = cursor_user.fetchall()
-
-        if len(results) == 0:
-           flash("Dane logowania są niepoprawne. Spróbuj ponownie")
-           return redirect(url_for('login'))
+        user = Users.query.filter_by(username=username).first()
+        if user:
+            if check_password_hash(user.password, password):
+                login_user(user)
+                return redirect(url_for('dashboard'))
+            else:
+                flash("Błędne hasło. Spróbuj ponownie")
+                return redirect(url_for('login'))
         else:
-            return redirect(url_for('home'))
+            flash("Użytkownik o takiej nazwie nie istnieje")
     return render_template('login.html')
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == "POST":
-        connection_user = sqlite3.connect('users_dane.db')
-        cursor_user = connection_user.cursor(); 
-
+        
         username = request.form['username']
         password = request.form['password']
-        query = "SELECT username FROM users WHERE username='"+username+"'"
-        cursor_user.execute(query)
 
-        results = cursor_user.fetchall()
-        if len(results) == 0:
-            add_to_base = "INSERT INTO users VALUES('"+username+"', '"+password+"')"
-            cursor_user.execute(add_to_base)
-            connection_user.commit()
-            return render_template('login.html')
-        else:
+        user = Users.query.filter_by(username=username).first()
+
+        if user:
             flash("Użytkownik o takiej nazwie już istnieje. Spróbuj ponownie")
             return redirect(url_for('signup'))
+        
+        new_user = Users(username=username, password=generate_password_hash(password, method='sha256'))
+
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
     return render_template('signup.html')
 
 @app.route('/home')
+@login_required
 def home():
     pol_def = str(rows[0][1])
     hiszp_def = str(rows[0][2])
     return render_template('home.html', pol_def=pol_def, hiszp_def=hiszp_def)
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/change-password', methods=['GET', 'POST'])
+@login_required
+def changePassword():
+    if request.method == 'POST':
+
+        user_update = Users.query.get_or_404(current_user.id)
+        #old_password = generate_password_hash(request.form['old_password'], method='sha256')
+        old_password = request.form['old_password']
+        new_password = request.form['new_password']
+        new_password_repeat = request.form['new_password_repeat']
+
+
+        if check_password_hash(current_user.password, old_password):
+            if new_password == new_password_repeat:
+                user_update.password = generate_password_hash(new_password)
+                db.session.commit()
+                flash("Hasło zostało zmienione")
+                return redirect(url_for('dashboard'))
+            else:
+                flash("Hasło i powtórzone hasło są różne")
+                return redirect(url_for('changePassword'))
+        else:
+            flash("Błędne hasło. Spróbuj ponownie.")
+            return redirect(url_for('changePassword'))
+        
+    return render_template('change_password.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ static/javascript/script.js
@@ -0,0 +1,4 @@
+function RollDownUserMenu(){
+    document.getElementById('user-menu').classList.toggle("user-menu");
+    document.getElementById('user-menu').classList.toggle("hidden");
+}
